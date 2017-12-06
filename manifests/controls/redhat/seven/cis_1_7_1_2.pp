68901d6fd27811db47ceb3362d7c7bb03b4f4ee5
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2017-12-06 23:32:56 +0100

    same change

--- manifests/controls/redhat/seven/cis_1_7_1_2.pp
+++ manifests/controls/redhat/seven/cis_1_7_1_2.pp
@@ -2,13 +2,15 @@ class demo_cis::controls::redhat::seven::cis_1_7_1_2 (
   $enable = true
 ){
   if $enable == true {
-    file {'cis_1_7_1_2_issue':
-      ensure  => present,
-      path    => '/etc/issue',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => 'Authorized uses only. All activity may be monitored and reported.',
+    if !defined(File['/etc/issue']) {
+      file {'cis_1_7_1_2_issue':
+        ensure  => present,
+        path    => '/etc/issue',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => 'Authorized uses only. All activity may be monitored and reported.',
+      }
     }
   }
 
