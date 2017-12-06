5583e70edd5936f8c623a2e60c4f5d2ef3dfeb0e
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2017-12-06 23:28:10 +0100

    prevent creating /etc/issue if it's already being done elsewhere (for example, the baseline profile)

--- manifests/controls/redhat/seven/cis_1_7_1_3.pp
+++ manifests/controls/redhat/seven/cis_1_7_1_3.pp
@@ -2,13 +2,15 @@ class demo_cis::controls::redhat::seven::cis_1_7_1_3 (
   $enable = true
 ){
   if $enable == true {
-    file {'cis_1_7_1_3_issue.net':
-      ensure  => present,
-      path    => '/etc/issue.net',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => 'Authorized uses only. All activity may be monitored and reported.',
+    if !defined(File['/etc/issue']) {
+      file { 'cis_1_7_1_3_issue.net':
+        ensure  => present,
+        path    => '/etc/issue.net',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => 'Authorized uses only. All activity may be monitored and reported.',
+      }
     }
   }
 
